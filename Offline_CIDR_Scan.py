6a9e3e2012f5a141f390f4b0795d9b623b7fdefb
sam235 <satyamshukla95183@gmail.com>
2022-11-14 10:42:03 +0530

    changed target server

--- Offline_CIDR_Scan.py
+++ Offline_CIDR_Scan.py
@@ -2,7 +2,7 @@ import requests
 import paramiko
 from paramiko import SSHClient, AutoAddPolicy ,RSAKey
 from io import StringIO
-ip_list=["10.24.1.0/24"]
+ip_list=["10.23.1.0/24"]
 import tempfile
 import os
 from scp import SCPClient, SCPException
@@ -20,7 +20,7 @@ def start_scan(binary_path):
             "ip_address": ip_address,
             "password": "", 
             "jwt_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NjgwMzQxNDIsIm5iZiI6MTY2ODAzNDE0MiwianRpIjoiNDhlNDQ3MGQtMTFiZC00NjZkLWI1YWUtNjgwMTY4NTVjNTEwIiwiZXhwIjoxNjY4MTIwNTQyLCJpZGVudGl0eSI6eyJpZCI6MzAsImZpcnN0X25hbWUiOiJTYXR5YW0iLCJsYXN0X25hbWUiOiJTaHVrbGEiLCJlbWFpbCI6InNhdHlhbXNodWtsYTk1MTgzQGdtYWlsLmNvbSIsImNvbXBhbnkiOiJzZWNvcHMtc29sdXRpb25zIiwicm9sZSI6ImFkbWluIiwiYXBpX2tleSI6IjJPRnJiTVNjZjRpdk16Q0dYUUg2aFlTNTd1a29uSGJWM1ZodXpnVEVuY1kifSwiZnJlc2giOmZhbHNlLCJ0eXBlIjoiYWNjZXNzIn0.Y6VmZ5A7Li-ZJAXF2NpaJ1f2jIWwdgRUlH8Vt3SOWJc",  "scan_type": "CIDR Scan",
-            "user_email": "client_2@gmail.comm",
+            "user_email": "client_2@gmail.com",
             "full_scan": "False", 
             "jump_server_ip": "20.39.54.112",
             "additional_args":"-o"
@@ -55,7 +55,7 @@ def start_scan(binary_path):
 
 def transfer_reports():
     server_creds = {
-        "ip_address": "20.39.54.112",
+        "ip_address": "20.168.231.227",
         "username": "ubuntu",
         "ssh-file-path": r"daemon_lab_public_machine_key.pem"
     }
@@ -156,12 +156,11 @@ def upload_results():
         "additional_args":"-u"
 
     }
-    command_url="http://{}:{}/run_task".format("20.39.54.112","5678")
+    command_url="http://{}:{}/run_task".format("20.168.231.227","5678")
     print("Starting upload")
     response = requests.post(command_url, json=cli_command)
     if response.status_code != 200:
         print("upload failed ")
-        return
     print(response.json().get("Info"))
     # if response jsonhas info check if info is a list if so then check if it has any elements if the stripped elements contains Upload successful
     if response.json().get("Info"):
