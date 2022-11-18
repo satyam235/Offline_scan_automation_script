21565d8cd8827c07b9d5821a84025e16ea0b9ae9
sam235 <satyamshukla95183@gmail.com>
2022-11-18 09:21:43 +0530

    improved code

--- Offline_CIDR_Scan.py
+++ Offline_CIDR_Scan.py
@@ -9,6 +9,11 @@ from scp import SCPClient, SCPException
 import json
 import subprocess
 from pathlib import Path
+import argparse
+from rich.console import Console
+
+console = Console()
+args = None
  
 directory = "/usr/local/bin/"
 
@@ -18,10 +23,27 @@ def get_home_directory():
 
 home_dir = get_home_directory() + "/"
 
+def printer(msg, fail=False):
+    if not args:
+        disable_color = True
+    else:
+        disable_color = False
+    if not disable_color:
+        if not fail:
+            console.print(msg, style = "green")
+        else:
+            console.print(msg, style = "red")
+    else:
+        print(msg)
+
+
 def start_scan(binary_path):
-    print("Initiating Scan on {} servers".format(len(ip_list)))
+    verbose = args.verbose
+    if verbose:
+        printer("Initiating Scan on {} servers".format(len(ip_list)))
     for ip_address in ip_list:
-        print("Scanning {}".format(ip_address))
+        if verbose:
+            printer("Scanning {}".format(ip_address))
         cli_command = {
             "operation": "remote_scan",
             "ip_address": ip_address,
@@ -49,42 +71,58 @@ def start_scan(binary_path):
         cli_process = subprocess.Popen([binary_path, "-cm", argument_dict]+additional_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, error = cli_process.communicate()
         if error:
-            print("Error in scan {}".format(error))
+            printer("Error in scan {}".format(error),True)
             return False
-        print(output)
-        print("Scan completed on {}".format(ip_address))
-    print("Scan Task for remote cli scan completed for user {}".format("Ascent"))
+        if args.debug:
+            printer("Output of scan {}".format(output))
+        if verbose:
+            printer("Scan completed on {}".format(ip_address))
+    printer("Scan Task for remote cli scan completed for user {}".format("Ascent"))
+    print("----------------------------------------")
     transfer_status = transfer_reports()
     return transfer_status
 
 def transfer_reports():
-    server_creds = {
-        "ip_address": "20.168.231.227",
-        "username": "ubuntu",
-        "ssh-file-path": r"daemon_lab_public_machine_key.pem"
-    }
-    ssh_client = get_ssh_client(server_creds, timeout=30)
-    if not ssh_client:
-        print("Failed to connect to server {}".format(server_creds.get("ip_address")))
+    try:
+        verbose = args.verbose
+        debug = args.debug
+        if verbose:
+            printer("Initiating transfer of reports")
+        server_creds = {
+            "ip_address": "20.168.231.227",
+            "username": "ubuntu",
+            "ssh-file-path": r"daemon_lab_public_machine_key.pem"
+        }
+        ssh_client = get_ssh_client(server_creds, timeout=30)
+        if not ssh_client:
+            printer("Failed to connect to server {}".format(server_creds.get("ip_address")),True)
+            return False
+        else:
+            # get home dir from server
+            stdin,stdout,stderr=ssh_client.exec_command("echo $HOME")
+            stdout.channel.recv_exit_status()
+            home_dir = stdout.read().decode("utf-8").strip()
+            if debug:
+                printer("Home dir of server is {}".format(home_dir))
+            if verbose:
+                printer("Connected to server {}".format(server_creds.get("ip_address")))
+            
+            scp_put_data(ssh_client, "/etc/secops_cli", home_dir)
+            command = "sudo cp -r {}/secops_cli /etc".format(home_dir)
+            ssh_execute_command(command,ssh_client)
+            command = "sudo rm -rf {}/secops_cli".format(home_dir)
+            ssh_execute_command(command,ssh_client)
+            ssh_client.close()
+            command = "sudo rm -rf /etc/secops_cli/offline_reports"
+            subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            if verbose:
+                printer("Transfer of reports completed")
+            print("----------------------------------------")
+            return True
+    except Exception as e:
+        printer("Error in transfer of reports {}".format(e),True)
         return False
-    else:
-        # get home dir from server
-        stdin,stdout,stderr=ssh_client.exec_command("echo $HOME")
-        stdout.channel.recv_exit_status()
-        home_dir = stdout.read().decode("utf-8").strip()
-        print("Home dir is {}".format(home_dir))
-        print("Successfully connected to server {}".format(server_creds.get("ip_address")))
-        scp_put_data(ssh_client, "/etc/secops_cli", home_dir)
-        command = "sudo cp -r {}/secops_cli /etc".format(home_dir)
-        ssh_execute_command(command,ssh_client)
-        command = "sudo rm -rf {}/secops_cli".format(home_dir)
-        ssh_execute_command(command,ssh_client)
-        ssh_client.close()
-        command = "sudo rm -rf /etc/secops_cli/offline_reports"
-        subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print("Successfully transferred the reports to the jump server")
-        return True
-
+    
 def get_ssh_client(server_creds, timeout=10):
     ssh_client = ssh_connect_private_key(
         server_creds["ip_address"],
@@ -96,6 +134,7 @@ def get_ssh_client(server_creds, timeout=10):
     return ssh_client
 
 def ssh_connect_private_key(ip_address, username, ssh_key, passphrase=None, timeout=None):
+    debug = args.debug
     try:
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(AutoAddPolicy())
@@ -107,33 +146,42 @@ def ssh_connect_private_key(ip_address, username, ssh_key, passphrase=None, time
         return client
         
     except Exception as ex:
-        print(ex)
-        print("Failed to connect to ssh client using key")
+        if debug:
+            printer("Error in connecting to server {}".format(ex),True)
         return None
 
 def scp_put_data(ssh_client, files, remote_path, recursive=False):
+    debug = args.debug
     try:
         scp_client = SCPClient(ssh_client.get_transport())
         scp_client.put(files,remote_path,True)
+        if debug:
+            printer("Successfully transferred files to server")
         print("Uploaded files %s remotely at path %s",files,remote_path)
     except SCPException as ex:
-       print("Error uploading files %s remotely at path \
-                        %s. Exception : %s ", files, remote_path, str(ex))
-       
+        if debug:
+            printer("Failed to transfer files to server , Exception".format(str(ex)),True)
     finally:
         if scp_client:
             scp_client.close()
 
 def ssh_execute_command(command, ssh_client):
+    debug = args.debug
     try:
+        if args.debug:
+            printer("Executing command {}".format(command))
         stdin,stdout,stderr=ssh_client.exec_command(command)
         stdout.channel.recv_exit_status()
-        print("Executed remote command %s succesfully",command)
+        if debug:
+            printer("Command executed successfully")
+        
     except SSHException as ex:
-        print("Error executing remote command %s. Exception: %s",command, str(ex))
+        if debug:
+            printer("Failed to execute command {} Exception {}".format(command,str(ex)),True)
         raise Forbidden("SSH exec command error ", str(ex))
 
 def check_binary():
+    debug = args.debug
     if os.name == "nt":
         binary_path = directory + os.sep + "secops_cli_windows-latest.exe"
         # check if binary exists
@@ -143,12 +191,17 @@ def check_binary():
         dir_files = os.listdir(directory)
         if "secops_cli_ubuntu-20.04" in dir_files:
             binary_name = "secops_cli_ubuntu-20.04"
-            print("Found binary for ubuntu 20.04")
+            if debug:
+                printer("Binary name is {}".format(binary_name))
+            
         elif "secops_cli_ubuntu-18.04" in dir_files:
             binary_name = "secops_cli_ubuntu-18.04"
-            print("Found binary for ubuntu 18.04")
+            if debug:
+                printer("Binary name is {}".format(binary_name))
+            
         else:
-            print("No binary found for ubuntu")
+            if debug:
+                printer("No binary found in the directory",True)
             return False
 
         binary_path = os.path.join(directory, binary_name)
@@ -156,50 +209,79 @@ def check_binary():
     return binary_path
 
 def upload_results():
-    #upload the results to the server
-    cli_command = {
-        "operation":"upload",
-        "acc_username":"client_2@gmail.com",
-        "acc_password":"Test@1234",
-        "acc_api_key":"vx7eLUSnq0ze9B1LuwCfjjTzy2TVfXSCI5IhOPne0fk",
-        "additional_args":"-u"
+    try:
+        #upload the results to the server
+        verbose = args.verbose
+        debug = args.debug
+        if verbose:
+            printer("Initiating upload of results")
+        cli_command = {
+            "operation":"upload",
+            "acc_username":"client_2@gmail.com",
+            "acc_password":"Test@1234",
+            "acc_api_key":"vx7eLUSnq0ze9B1LuwCfjjTzy2TVfXSCI5IhOPne0fk",
+            "additional_args":"-u"
 
-    }
-    command_url="http://{}:{}/run_task".format("20.168.231.227","5678")
-    #command_url="https://{}:{}/run_task".format("20.168.231.227","5678")
-    print("Starting upload")
-    response = requests.post(command_url, json=cli_command)
-    if response.status_code != 200:
-            command_url="http://{}:{}/run_task".format("20.168.231.227","5678")
-            print("Sending command to hop server {}".format(command_url))
-            response = requests.post(command_url, json=cli_command)
-            if response.status_code != 200:
-                print("upload failed ")
-                print("Error while sending command to hop server {} -- {}".format(hop_server_ip, response.text))
-                return
+        }
+        command_url="http://{}:{}/run_task".format("20.168.231.227","5678")
         
-    print(response.json().get("Info"))
-    # if response jsonhas info check if info is a list if so then check if it has any elements if the stripped elements contains Upload successful
-    if response.json().get("Info"):
-        if isinstance(response.json().get("Info"), list):
-            if any("Upload successful" in s.strip()  for s in response.json().get("Info")):
-                print("upload successful ")
+        if debug:
+            printer("Command url is {}".format(command_url))
+            printer(("CLI Command is {}".format(cli_command)))
+        response = requests.post(command_url, json=cli_command)
+        if response.status_code != 200:
+                command_url="http://{}:{}/run_task".format("20.168.231.227","5678")
+                if debug:
+                    printer("Command url is {}".format(command_url))
+                    printer(("CLI Command is {}".format(cli_command)))
+                
+                response = requests.post(command_url, json=cli_command)
+                if response.status_code != 200:
+                    if debug:
+                        printer("Failed to upload results , {}".format(response.text),True)
+                    return
+        if debug:
+            printer("Response from server {}".format(response.text))
+        
+        # if response jsonhas info check if info is a list if so then check if it has any elements if the stripped elements contains Upload successful
+        if response.json().get("Info"):
+            if isinstance(response.json().get("Info"), list):
+                if any("Upload successful" in s.strip()  for s in response.json().get("Info")):
+                    if verbose:
+                        printer("Upload successful")
+                        print("----------------------------------------")
+                    return True
+                else:
+                    if verbose:
+                        printer("Upload failed",True)
+                    return False
             else:
-                print("upload failed ")
-                return
+                if "Upload successful" in response.json().get("Info").strip():
+                    if verbose:
+                        printer("Upload successful")
+                        print("----------------------------------------")
+                    return True
+                else:
+                    if verbose:
+                        printer("Upload failed",True)
+                    return False
         else:
-            if "Upload successful" in response.json().get("Info").strip():
-                print("upload successful ")
-            else:
-                print("upload failed ")
-                return
-    else:
-        print("upload failed , No info found in response")
-        return
-
+            if verbose:
+                printer("Upload failed,please try again in sometime.",True)
+            return
+    except Exception as e:
+        if debug:
+            printer("Exception occured during  upload {}".format(e),True)
+        return False
 
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Build the secops cli')
+    parser.add_argument('-d','--debug', action='store_true', help='Enable debug mode')
+    parser.add_argument('-v', '--verbose', help='Verbose output', action='store_true',default=True)
+    args = parser.parse_args()
+    sucecss = False
     binary_path = check_binary()
     if binary_path:
         sucecss = start_scan(binary_path)
